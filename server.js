891f8cc765fa2a136463ea11380038ad45f896e3
ottidditto <metamong7647@naver.com>
2023-02-27 20:08:38 +0900

    serverjs : routing setting

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const app = express();
 
 // client-side rendering : 프론트 빌드파일을 가져와서 서버에서 html렌더링
 app.use(express.static(path.join(__dirname, 'fullhouse-mall-front/build')));
-app.get('/', function (요청, 응답) {
-  응답.sendFile(path.join(__dirname, '/fullhouse-mall-front/build/index.html'));
+
+// 라우팅은 react-router-dom에서 수행하도록 설정되어있음
+app.get('*', function (request, response) {
+  response.sendFile(path.join(__dirname, '/react-project/build/index.html'));
 });
 
 // 실행 : node server.js
